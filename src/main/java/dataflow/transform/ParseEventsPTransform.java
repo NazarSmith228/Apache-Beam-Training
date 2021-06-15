6551779fa16e1ec4ff415fea24e9cce990971f93
Nazar_Koval1 <nazarmarmeladka228@gmail.com>
2021-06-15 12:41:21 +0300

    Complete data processing pipeline

---
+++ src/main/java/dataflow/transform/ParseEventsPTransform.java
@@ -0,0 +1,19 @@
+package dataflow.transform;
+
+import dataflow.model.Event;
+import org.apache.beam.sdk.coders.AvroCoder;
+import org.apache.beam.sdk.extensions.jackson.ParseJsons;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PCollection;
+
+public class ParseEventsPTransform extends PTransform<PCollection<String>, PCollection<Event>> {
+    @Override
+    public PCollection<Event> expand(PCollection<String> input) {
+        return input.apply("Parse strings to Event.class",
+                ParseJsons.of(Event.class)
+        )
+                .setCoder(
+                        AvroCoder.of(Event.class, Event.getClassSchema())
+                );
+    }
+}
